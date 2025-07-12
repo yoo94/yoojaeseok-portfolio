ba79be22826280f1cb84d0bdf48293b41f839703
YOOJAESEOK <jaeseok9405@gmail.com>
2025-07-12 23:06:03 +0900

    fix: error fix

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,6 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { createNavigation } from 'next-intl/navigation';
 
 export const locales = ['ko', 'en'] as const;
-export const localePrefix = 'always'; // 또는 'as-needed'
 
 export const { Link, redirect, usePathname, useRouter } = 
-  createSharedPathnamesNavigation({ locales, localePrefix });
+  createNavigation({ locales });
